560360a21b28e29b5bea14200c98e17cf261742b
Jainil Patel <128704632+amada04@users.noreply.github.com>
2025-04-01 18:13:28 -0400

    Update Tracker.cpp

--- Tracker.cpp
+++ Tracker.cpp
@@ -8,17 +8,22 @@ double heading;
   
 class Tracker:
 
-  Tracker () {
-    Serial.begin(9600);
-    pinMode(trig1, OUTPUT);
-    pinMode(echo1, INPUT);
-  }
   public:
-    Coordinate ping() {
+    Tracker () {
+      Serial.begin(9600);
+      pinMode(trig1, OUTPUT);
+      pinMode(echo1, INPUT);
+      pinMode(R_Sensor, INPUT);
+      pinMode(RF_Sensor, INPUT);
+      pinMode(L_Sensor, INPUT);
+      pinMode(LF_Sensor, INPUT);
+    }
 
+    
+    void ping() {
       // create distance, duration, and delta heading variables
       double distance, duration;
-      double dHeading;
+      double dHeading = 0;
 
       // trigger sensor
       digitalWrite(trig, LOW);
@@ -57,9 +62,63 @@ class Tracker:
       target.heading = FuzeBot.heading + dHeading;     
     }
 
-    void move_to (Coordinate target) {}
+    void move_to (Coordinate target) {
+      
+    }
+
     void track () {
       
   private:
     Position FuzeBot;
     Coordinate target;
+    Coordinate lookAhead;
+    double lookAheadDistance = 8;
+
+    void findLookAhead() {
+
+      // get current x and y
+      double x = FuzeBot.getX();
+      double y = FuzeBot.geyY();
+      
+      // get target x and y
+      double p1x = target.x;
+      double p2x = target.y;
+
+      // calculate discriminant
+      double a = pow((p1x - x), 2) + pow((p1y - y), 2);
+      double c = -pow(lookAheadDistance, 2);
+
+      if (-c / a >= 0) {
+        double t = sqrt(-c/a);
+
+        // calculate intersection
+        double lx = x + t * (p1x - x);
+        double ly = y + t * (p11y -y);
+        
+        // assign to look ahead coordinates
+        if (t <= 1 && sqrt(pow(lx - x, 2) + pow(ly - y, 2)) >= 0.001) {
+          lookAhead.x = lx;
+          lookAhead.y = ly;
+        } else {
+          lookAhead.x = -1;
+          lookAhead.y = -1;
+        } 
+      }
+
+      void findCurvature() {
+
+        if (lookAhead.x == -1 && lookAhead.y == -1) {
+          lookAhead.heading = -1;
+          return;
+        }
+        
+        double x = FuzeBot.getX();
+        double Y = FuzeBot.getY();
+        double heading = FuzeBot.getHeading();
+        
+        double alpha = atan2(ly -y, lx - x) - heading;
+        double curvature = 2 * sin(alpha) / lookAheadDistance;
+      }
+        
+    }
+    
