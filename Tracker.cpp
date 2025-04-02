8e32af9b12941f28fc85a5dd8f0e58c86ac0148b
Jainil Patel <128704632+amada04@users.noreply.github.com>
2025-04-02 14:44:29 -0400

    Update Tracker.cpp

--- Tracker.cpp
+++ Tracker.cpp
@@ -1,186 +1,259 @@
-#include 'Tracker.hpp'
+#include "Tracker.hpp"
+#include <cmath>
 
-struct Coordinate {
-double x;
-double y;
-double heading;
+Tracker::Tracker() :
+  trig1(12), echo1(13), R_Sensor(2), RF_Sensor(3), L_Sensor(4), LF_Sensor(5),
+  CONTACT_PIN(6), LIFTED_PIN(7), WHITE_LINE_PIN(8) {
+
+  Serial.begin(9600);
+  pinMode(trig1, OUTPUT);
+  pinMode(echo1, INPUT);
+  pinMode(R_Sensor, INPUT);
+  pinMode(RF_Sensor, INPUT);
+  pinMode(L_Sensor, INPUT);
+  pinMode(LF_Sensor, INPUT);
+  pinMode(CONTACT_PIN, INPUT);
+  pinMode(LIFTED_PIN, INPUT);
+  pinMode(WHITE_LINE_PIN, INPUT);
+
+  lastTargetTime = 0;
+  lastTargetX = 0;
+  lastTargetY = 0;
+  isTargetFound = false;
+  isTargetLost = true;
+  escape_l = false;
+  escape_r = false;
+  escape_b = false;
 }
-  
-class Tracker:
 
-  public:
-    Tracker () {
-      Serial.begin(9600);
-      pinMode(trig1, OUTPUT);
-      pinMode(echo1, INPUT);
-      pinMode(R_Sensor, INPUT);
-      pinMode(RF_Sensor, INPUT);
-      pinMode(L_Sensor, INPUT);
-      pinMode(LF_Sensor, INPUT);
-      lastTargetTime = 0;
-      lastTargetX = 0;
-      lastTargetY = 0;
+void Tracker::setEscapeConditions(bool left, bool right, bool back) {
+  escape_l = left;
+  escape_r = right;
+  escape_b = back;
+}
+
+void Tracker::ping() {
+  calculateTargetPosition();
+  predictTargetPosition();
+  updateContactStatus();
+  updateLiftedStatus();
+}
+
+void Tracker::move_to(Coordinate target) {
+  while (true) {
+    findLookAhead();
+    double curvature = findCurvature();
+
+    if (curvature == -1) {
+      Serial.println("Target reached or no lookahead point.");
+      break;
     }
 
-    
-    void ping() {
-      // create distance, duration, and delta heading variables
-      double distance, duration;
-      double dHeading = 0;
+    double steering_angle = atan(curvature * WHEELBASE);
+    applyMotorControl(steering_angle);
 
-      // trigger sensor
-      digitalWrite(trig, LOW);
-      delayMicroseconds(2);
-      digitalWrite(trig, HIGH);
-      delayMicroseconds(10);
-      duration = pulseIn(echo, HIGH, 3000);
+    updateRobotPosition();
 
-      // calculate distance
-      distance = (duration/2) * 0.0343;
-
-      // if distance is within the max diameter of the field (cm)
-      if (distance < 76.2) {
-
-        // comb through all IR sensor combination readings and assign a change in heading
-        if ((digitalRead(R_Sensor) == HIGH) && (digitalRead(RF_Sensor) == HIGH) && (digitalRead(LF_Sensor) == HIGH)) {
-          dHeading = 10;
-        } else if ((digitalRead(L_Sensor) == HIGH) && (digitalRead(LF_Sensor) == HIGH) && (digitalRead(RF_Sensor) == HIGH)) {
-          dHeading -10;
-        } else if ((digitalRead(R_Sensor) == HIGH) && (digitalRead(RF_Sensor) == HIGH)) {
-          dHeading = 20;
-        } else if ((digitalRead(L_Sensor) == HIGH) && (digitalRead(LF_Sensor) == HIGH)) {
-          dHeading = -20;
-        } else if ((digitalRead(RF_Sensor) == HIGH) && (digitalRead(LF_Sensor) == HIGH)) {
-          dHeading = 0;
-        } else if (digitalRead(R_Sensor) == HIGH) {
-          dHeading = 90;
-        } else if (digitalRead(RF_Sensor) == HIGH) {
-          dHeading = 20;
-        } else if (digitalRead(L_Sensor) == HIGH) {
-          dHeading = -90;
-        } else if (digitalRead(LF_Sensor) == HIGH) {
-          dHeading = -20;
-        }
-      }
-
-      // using calculated distance and dHeading calculate target coordinates
-      target.x = FuzeBot.getX() + (distance * sin(heading));
-      target.y = FuzeBot.getY() + (distance * cos(heading));
-      target.heading = FuzeBot.heading + dHeading;   
-
-      // get current time
-      unsigned long currentTime = millis();
-      if (lastTargetTime != 0) {
-
-        // calculate target velocities
-        double deltaTime = (currentTime - lastTargetTime) / 1000.0;
-        double targetVelX = (target.x - lastTargetX) / deltaTime;
-        double targetVelY = (target.y - lastTargetY) / deltaTime;
-        lastTargetTime = currentTime;
-
-        // predict future position
-        target.x += targetVelX * predictionTime;
-        target.y += targetVelY * predictionTime;
-      } else {
-        
-        // initialize lastTarget on the first reading.
-        lastTargetX = target.x;
-        lastTargetY = target.y;
-        lastTargetTime = currentTime;
-      }
-      
+    if (isTargetReached(target)) {
+      Serial.println("Target reached!");
+      stopMotors();
+      break;
     }
+  }
+}
 
-    void move_to (Coordinate target) {
-
-      // while true
-      while (true) {
-
-        // get look ahead point and curvature
-        findLookAhead();
-        double curvature = findCurvature();
-
-        if (curvature == -1) {
-          Serial.println("Target reached or no lookahead point.");
-          break;
-        }
-
-        // calculate angle
-        double steering_angle = atan(curvature * 10.0); // Adjust wheelbase
-      // ... your motor control code here ...
-        Serial.print("Curvature: ");
-        Serial.print(curvature);
-        Serial.print(" Steering angle: ");
-        Serial.println(steering_angle);
-
-        // 
-        double delta_x = cos(FuzeBot.getHeading()) * 1.0;
-        double delta_y = sin(FuzeBot.getHeading()) * 1.0;
-        double delta_heading = steering_angle * 0.1;
-        FuzeBot.updatePosition(delta_x, delta_y, delta_heading);
-        delay(50);
-
-        double distance_to_target = sqrt(pow(target.x - FuzeBot.getX(), 2) + pow(target.y - FuzeBot.getY(), 2));
-        if (distance_to_target < 5.0) {
-          Serial.println("Target reached!");
-          break;
-        }
-      }
+void Tracker::knockout() {
+  setKnockoutSpeed();
+  while (digitalRead(WHITE_LINE_PIN) == LOW) {
+    applyMotorControl(0);
+    updateRobotPosition();
+    if (isTargetDetectedWithin10cm()) {
+      break;
     }
-      
-  private:
-    Position FuzeBot;
-    Coordinate target;
-    Coordinate lookAhead;
-      unsigned long lastTargetTime;
-    double lastTargetX;
-    double lastTargetY;
-    double predictionTime = 0.2;
+  }
+  stopMotors();
+}
 
-    void findLookAhead() {
+void Tracker::escape() {
+  if (escape_l) {
+    moveToSafePosition(ESCAPE_LEFT);
+  } else if (escape_r) {
+    moveToSafePosition(ESCAPE_RIGHT);
+  } else if (escape_b) {
+    moveToSafePosition(ESCAPE_BACK);
+  }
 
-      // get current x and y
-      double x = FuzeBot.getX();
-      double y = FuzeBot.geyY();
-      
-      // get target x and y
-      double p1x = target.x;
-      double p2x = target.y;
+  if (isContact()) {
+    knockout();
+  }
+}
 
-      // calculate discriminant
-      double a = pow((p1x - x), 2) + pow((p1y - y), 2);
-      double c = -pow(lookAheadDistance, 2);
+void Tracker::calculateTargetPosition() {
+  double distance, duration;
+  double veer = 0;
 
-      if (-c / a >= 0) {
-        double t = sqrt(-c/a);
+  digitalWrite(trig1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig1, HIGH);
+  delayMicroseconds(10);
+  duration = pulseIn(echo1, HIGH, 30000);
 
-        // calculate intersection
-        double lx = x + t * (p1x - x);
-        double ly = y + t * (p11y -y);
-        
-        // assign to look ahead coordinates
-        if (t <= 1 && sqrt(pow(lx - x, 2) + pow(ly - y, 2)) >= 0.001) {
-          lookAhead.x = lx;
-          lookAhead.y = ly;
-        } else {
-          lookAhead.x = -1;
-          lookAhead.y = -1;
-        } 
-      }
+  distance = (duration / 2.0) * 0.0343;
 
-      double findCurvature() {
-
-        if (lookAhead.x == -1 && lookAhead.y == -1) {
-          return -1;
-        }
-        
-        double x = FuzeBot.getX();
-        double Y = FuzeBot.getY();
-        double heading = FuzeBot.getHeading();
-        
-        double alpha = atan2(ly -y, lx - x) - heading;
-        double curvature = 2 * sin(alpha) / lookAheadDistance;
-      }
-        return curvature;
+  if (distance < 76.2 && duration > 0) {
+    if ((digitalRead(R_Sensor) == HIGH) && (digitalRead(RF_Sensor) == HIGH) && (digitalRead(LF_Sensor) == HIGH)) {
+      veer = 10;
+    } else if ((digitalRead(L_Sensor) == HIGH) && (digitalRead(LF_Sensor) == HIGH) && (digitalRead(RF_Sensor) == HIGH)) {
+      veer = -10;
+    } else if ((digitalRead(R_Sensor) == HIGH) && (digitalRead(RF_Sensor) == HIGH)) {
+      veer = 20;
+    } else if ((digitalRead(L_Sensor) == HIGH) && (digitalRead(LF_Sensor) == HIGH)) {
+      veer = -20;
+    } else if ((digitalRead(RF_Sensor) == HIGH) && (digitalRead(LF_Sensor) == HIGH)) {
+      veer = 0;
+    } else if (digitalRead(R_Sensor) == HIGH) {
+      veer = 90;
+    } else if (digitalRead(RF_Sensor) == HIGH) {
+      veer = 20;
+    } else if (digitalRead(L_Sensor) == HIGH) {
+      veer = -90;
+    } else if (digitalRead(LF_Sensor) == HIGH) {
+      veer = -20;
     }
-    
+  }
+
+  double veerRad = veer * M_PI / 180.0;
+  target.x = FuzeBot.getX() + (distance * cos(FuzeBot.getHeading() + veerRad));
+  target.y = FuzeBot.getY() + (distance * sin(FuzeBot.getHeading() + veerRad));
+  target.heading = FuzeBot.getHeading() + veerRad;
+
+  if (distance > 0) {
+    isTargetFound = true;
+    isTargetLost = false;
+  } else {
+    isTargetFound = false;
+    isTargetLost = true;
+  }
+}
+
+void Tracker::predictTargetPosition() {
+  unsigned long currentTime = millis();
+  if (lastTargetTime != 0) {
+    double deltaTime = (currentTime - lastTargetTime) / 1000.0;
+    double targetVelX = (target.x - lastTargetX) / deltaTime;
+    double targetVelY = (target.y - lastTargetY) / deltaTime;
+    lastTargetTime = currentTime;
+    target.x += targetVelX * predictionTime;
+    target.y += targetVelY * predictionTime;
+  } else {
+    lastTargetX = target.x;
+    lastTargetY = target.y;
+    lastTargetTime = currentTime;
+  }
+}
+
+void Tracker::findLookAhead() {
+  double x = FuzeBot.getX();
+  double y = FuzeBot.getY();
+  double p1x = target.x;
+  double p1y = target.y;
+
+  double a = pow((p1x - x), 2) + pow((p1y - y), 2);
+  double c = -pow(lookAheadDistance, 2);
+
+  if (-c / a >= 0) {
+    double t = sqrt(-c / a);
+    double lx = x + t * (p1x - x);
+    double ly = y + t * (p1y - y);
+
+    if (t <= 1 && sqrt(pow(lx - x, 2) + pow(ly - y, 2)) >= 0.001) {
+      lookAhead.x = lx;
+      lookAhead.y = ly;
+    } else {
+      lookAhead.x = -1;
+      lookAhead.y = -1;
+    }
+  } else {
+    lookAhead.x = -1;
+    lookAhead.y = -1;
+  }
+}
+
+double Tracker::findCurvature() {
+  if (lookAhead.x == -1 && lookAhead.y == -1) {
+    return -1;
+  }
+
+  double x = FuzeBot.getX();
+  double y = FuzeBot.getY();
+  double heading = FuzeBot.getHeading();
+
+  double alpha = atan2(lookAhead.y - y, lookAhead.x - x) - heading;
+  double curvature = 2 * sin(alpha) / lookAheadDistance;
+  return curvature;
+}
+
+void Tracker::applyMotorControl(double steering_angle) {
+  double leftMotorSpeed = 100.0 - steering_angle * 50.0;
+  double rightMotorSpeed = 100.0 + steering_angle * 50.0;
+
+  leftMotorSpeed = constrain(leftMotorSpeed, -255, 255);
+  rightMotorSpeed = constrain(rightMotorSpeed, -255, 255);
+
+  setLeftMotorSpeed(leftMotorSpeed);
+  setRightMotorSpeed(rightMotorSpeed);
+}
+
+void Tracker::stopMotors() {
+  setLeftMotorSpeed(0);
+  setRightMotorSpeed(0);
+}
+
+void Tracker::updateRobotPosition() {
+  FuzeBot.updatePosition();
+  double currentHeading = getAccelerometerHeading();
+  if (isLifted()) {
+    FuzeBot.robotPosition.heading = currentHeading;
+  } else {
+    // Implement heading fusion here
+  }
+}
+
+bool Tracker::isTargetReached(Coordinate target) {
+  double distance_to_target = sqrt(pow(target.x - FuzeBot.getX(), 2) + pow(target.y - FuzeBot.getY(), 2));
+  return distance_to_target < 5.0;
+}
+
+bool Tracker::isContact() {
+  return digitalRead(CONTACT_PIN) == HIGH;
+}
+
+bool Tracker::isLifted() {
+  return digitalRead(LIFTED_PIN) == HIGH;
+}
+
+bool Tracker::isTargetDetectedWithin10cm() {
+  // Implement ultrasonic distance check for 10cm
+  return false;
+}
+
+void Tracker::setKnockoutSpeed() {
+  // Set knockout motor speeds
+}
+
+void Tracker::moveToSafePosition(EscapeDirection direction) {
+  // Implement movement to safe position based on direction
+}
+
+double Tracker::getAccelerometerHeading() {
+  // Implement accelerometer heading reading
+  return 0.0;
+}
+
+void Tracker::setLeftMotorSpeed(int speed) {
+  // Implement left motor control
+}
+
+void Tracker::setRightMotorSpeed(int speed) {
+  // Implement right motor control
+}
