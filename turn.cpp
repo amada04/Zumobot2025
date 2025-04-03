5ac6b9df270813a67db9057234ed4ae64cf6ffb6
Shahzad <144064520+Shahzadfuze@users.noreply.github.com>
2025-04-03 19:03:24 -0400

    Update turn.cpp

--- turn.cpp
+++ turn.cpp
@@ -1,64 +1,36 @@
-#include "gyroSensor.h"
+/* Function to turn robot to a specific angle */
+/*IT TURNSSSSSSSSSSSSSSSSSSSSSS YIPPPPY*/
 
-
-float Kp =0;
-float Ki = 0;
-float Kd = 0;
-
-
-float bias = 0;
-int baseSped = 200;
-
-
-/*Does this work?
-     Good question idk
-        We can test it out tomarrow,
-      */
-
-
-void turn(int16_t targetAngle){
-
-
-  if(targetAngle > 360){
+/*When the robot accelerates to much the gyro loses some persetion. But it doesn drift by its self anymore
+Now on to prosition tracting
+*/
+void turn(int16_t targetAngle) {
+  if (targetAngle > 360) {
     Serial.print("Error");
-    return 0;
+    return;
   }
-
-
-
-
-
 
   int16_t error = targetAngle;
   int16_t lastError = error;
-  float intergral = 0;
+  float integral = 0;
 
-
-  while(abs(error) > 5){
-
+  while (abs(error) > 5) {
     turnSensorUpdate();
 
-    int16_t currentAngle = (((int32_t)turnAngle >> 16) *360) >> 16;
+    int16_t currentAngle = (((int32_t)turnAngle >> 16) * 360) >> 16;
     error = targetAngle - currentAngle;
 
-    intergral += error;
-    intergral = constrain(intergral, -1000, 1000); // Intergral windup sucks
+    integral += error;
+    integral = constrain(integral, -1000, 1000); // Integral windup prevention
 
-    int16_t derivitive = error - lastError;
+    int16_t derivative = error - lastError;
     lastError = error;
 
-    int16_t turnSpeed = (Kp*error) + (Ki * intergral) +(Kd * derivitive);
-
-    turnSpeed = constrain(turnSpeed, -baseSpeed, baseSpeed); // limiter so we don't blow up the bot
+    int16_t turnSpeed = (Kp * error) + (Ki * integral) + (Kd * derivative);
+    turnSpeed = constrain(turnSpeed, -baseSpeed, baseSpeed);
 
     motors.setSpeeds(-turnSpeed, turnSpeed);
-
-
-
-
   }
 
-motors.setSpeed(0, 0);
-
-
-} 
+  motors.setSpeeds(0, 0);
+}
