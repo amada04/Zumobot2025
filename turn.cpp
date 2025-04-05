9d7e10f4ee0490a1a83f4c06ead28425e466205b
Jainil Patel <128704632+amada04@users.noreply.github.com>
2025-04-04 23:46:36 -0400

    Update turn.cpp

--- turn.cpp
+++ turn.cpp
@@ -13,7 +13,7 @@ float bias = 0;
 int baseSpeed = 200;
 
 
-void turn(int16_t turnAngle) {
+void turn(int16_t targetAngle) {
   if (targetAngle > 360) {
     Serial.print("Error");
     return;
@@ -21,8 +21,6 @@ void turn(int16_t turnAngle) {
 
   on_contact_lost();
 
-  int16_t currentAngle = (((int32_t)turnAngle >> 16) * 360) >> 16;
-  int16_t targetAngle = currentAngle + turnAngle;
   int16_t error = targetAngle;
   int16_t lastError = error;
   float integral = 0;
@@ -30,7 +28,7 @@ void turn(int16_t turnAngle) {
   while (abs(error) > 5) {
     turnSensorUpdate();
 
-    currentAngle = (((int32_t)turnAngle >> 16) * 360) >> 16;
+    currentAngle = (((int32_t)targetAngle >> 16) * 360) >> 16;
     error = targetAngle - currentAngle;
 
     integral += error;
